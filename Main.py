5ce5461a175e25c3b29825660170a41cf72d5d39
ermrk <ermrk@seznam.cz>
2016-08-23 12:25:01 +0200

    Change in structure of user input

--- Main.py
+++ Main.py
@@ -28,19 +28,24 @@ def send_post(url, text, context, state, session):
     return requests.post(url, data=json.dumps(payload), headers=headers)
 
 
+# tests dialogue
 def execute_test(loaded_yaml):
     i = 0
+    # while we are not at the end of yaml
     while i < len(loaded_yaml):
+        # if it is user's turn
         if loaded_yaml[i]["agent"] == "user":
-            if isinstance(loaded_yaml[i]["input"], list):
-                randomly_selected = select_random_input(loaded_yaml[i]["input"])
-                text_to_send = randomly_selected["text"]
+            # select one input at random
+            randomly_selected = select_random_input(loaded_yaml[i]["input"])
+            # take text and transition
+            text_to_send = randomly_selected["text"]
+            # take transition, if presented
+            if hasattr(randomly_selected, "transition"):
                 i = randomly_selected["transition"] - 1
-            else:
-                text_to_send = loaded_yaml[i]["input"]
             response = send_post(url, text_to_send, context, state, session)
             save_info_from_response(response)
             print(response.json())
+        # if it is Alquist's turn
         elif loaded_yaml[i]["agent"] == "alquist":
             if not (loaded_yaml[i]["text"] == response_text):
                 print("Mistake in the node " + state)
@@ -48,6 +53,7 @@ def execute_test(loaded_yaml):
         i += 1
 
 
+# save context, state, session and response from Alquist's request
 def save_info_from_response(response):
     global context
     global state
@@ -59,11 +65,14 @@ def save_info_from_response(response):
     response_text = response.json()['text']
 
 
+# select one input at random
 def select_random_input(inputs):
     index = randint(0, len(inputs) - 1)
     return inputs[index]
 
 
 if __name__ == '__main__':
+    # load yaml
     loaded_yaml = parse()
+    # test the dialogue
     execute_test(loaded_yaml)
